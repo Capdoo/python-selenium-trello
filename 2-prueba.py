e1789982b41c135f025a9cf4c157cca1d1e4b85a
Capdoo <mazher_2014@hotmail.com>
2022-01-26 09:49:44 -0500

    first commit

---
+++ 2-prueba.py
@@ -0,0 +1,54 @@
+#Importanción de librerias
+from selenium import webdriver
+import time
+
+TIEMPO_ESPERA = 3
+
+driver = webdriver.Chrome('./chromedriver')
+#1. Ingresar al home
+driver.get('https://trello.com/es')
+
+#2. Click en iniciar sesión
+driver.find_element_by_link_text("Iniciar sesión").click()
+time.sleep(TIEMPO_ESPERA)
+user_box = driver.find_element_by_name("user")
+pass_box = driver.find_element_by_name("password")
+submit_button = driver.find_element_by_id("login")
+
+
+user_box.send_keys("mazher_2014@hotmail.com")
+pass_box.send_keys("passwordpruebas")
+submit_button.click()
+
+time.sleep(TIEMPO_ESPERA)
+
+
+pass_box_2 = driver.find_element_by_name("password")
+pass_box_2.send_keys("passwordpruebas")
+submit_button_2 = driver.find_element_by_id("login-submit")
+submit_button_2.click()
+
+
+# PRIMERA PRUEBA : Dentro de la página principal
+
+time.sleep(TIEMPO_ESPERA)
+time.sleep(TIEMPO_ESPERA)
+driver.find_element_by_xpath('//*[@id="header"]/div[3]/div/div[2]/button/span/span').click()
+time.sleep(TIEMPO_ESPERA)
+
+time.sleep(TIEMPO_ESPERA)
+
+driver.find_element_by_xpath('/html/body/div[2]/div/section/div/nav/ul/li[1]/button/span[2]').click()
+#espacio_nombre.send_keys("nuevo espacio 1")
+
+time.sleep(TIEMPO_ESPERA)
+titulo_box = driver.find_element_by_xpath('/html/body/div[2]/div/section/div/form/div[1]/label/input')
+time.sleep(TIEMPO_ESPERA)
+titulo_box.send_keys("tablero_prueba_1")
+#driver.execute_script("arguments[0].innerText = 'Empresa pequeña'", select_combo)
+
+time.sleep(TIEMPO_ESPERA)
+
+driver.find_element_by_xpath('/html/body/div[2]/div/section/div/form/button').click()
+time.sleep(TIEMPO_ESPERA)
+
---
+++ main.py
@@ -0,0 +1,60 @@
+#Importanción de librerias
+from selenium import webdriver
+import time
+
+TIEMPO_ESPERA = 3
+
+driver = webdriver.Chrome('./chromedriver')
+#1. Ingresar al home
+driver.get('https://trello.com/es')
+
+#2. Click en iniciar sesión
+driver.find_element_by_link_text("Iniciar sesión").click()
+time.sleep(TIEMPO_ESPERA)
+user_box = driver.find_element_by_name("user")
+pass_box = driver.find_element_by_name("password")
+submit_button = driver.find_element_by_id("login")
+
+
+user_box.send_keys("mazher_2014@hotmail.com")
+pass_box.send_keys("passwordpruebas")
+submit_button.click()
+
+time.sleep(TIEMPO_ESPERA)
+
+
+pass_box_2 = driver.find_element_by_name("password")
+pass_box_2.send_keys("passwordpruebas")
+submit_button_2 = driver.find_element_by_id("login-submit")
+submit_button_2.click()
+
+
+# PRIMERA PRUEBA : Dentro de la página principal
+
+time.sleep(TIEMPO_ESPERA)
+driver.find_element_by_xpath('//*[@data-test-id="header-member-menu-button"]').click()
+time.sleep(TIEMPO_ESPERA)
+driver.find_element_by_link_text("Perfil y visibilidad").click()
+time.sleep(TIEMPO_ESPERA)
+
+
+name_box = driver.find_element_by_name("username")
+bio_box = driver.find_element_by_class_name("_2G_T79mYVTPcpm")
+
+time.sleep(TIEMPO_ESPERA)
+#submit_button = driver.find_element_by_class_name("_3cY-HMyZpdFgkA _3TTqkG5muwOzqZ _3Ik0JLsERwh6Ui _1Tu9wiuW4Te8Rx")
+submit_button = driver.find_element_by_xpath('//*[@id="content"]/div/div[3]/div[2]/div/div/div/form/button')
+
+
+
+time.sleep(TIEMPO_ESPERA)
+name_box.send_keys("2")
+bio_box.send_keys("Descripción de prueba : biografía")
+submit_button.click()
+
+
+time.sleep(TIEMPO_ESPERA)
+#driver.find_element_by_link_text("Guardar").click()
+
+
+#driver.quit()
